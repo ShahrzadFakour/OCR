d3c90056a43ad192429fe9b4e5ce5bc62d21aa8f
ShahrzadFakour <66277015+ShahrzadFakour@users.noreply.github.com>
2021-10-21 18:02:58 +0200

    Create training_network.py

---
+++ usingCNN/training_network.py
@@ -0,0 +1,40 @@
+from OCR_CNN import create_model, splitting_data,augmentation
+import numpy as np
+import tensorflow as tf
+import os
+import matplotlib.pyplot as plt
+from pathlib import Path
+from collections import Counter
+from tensorflow import keras
+from tensorflow.keras import layers,optimizers
+from tensorflow.keras.optimizers import Adam
+import cv2
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten
+from sklearn.preprocessing import LabelBinarizer
+from keras.preprocessing.image import ImageDataGenerator
+
+
+
+
+def training_model():
+    # initialize model
+
+    x_train, x_test, x_validation, y_train, y_test, y_validation=augmentation()
+    model1 = create_model()
+    # set training process parameters
+    batchSize = 256
+    epochs = 10000
+    print(y_train.shape)
+    label_as_binary = LabelBinarizer()
+    y_train = label_as_binary.fit_transform(y_train)
+    print(y_train.shape)
+    # Set the training configurations: optimizer, loss function, accuracy metrics
+    model1.compile(Adam(lr=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
+    history = model1.fit(x_train, y_train, batch_size=batchSize, epochs=epochs,
+                         validation_data=(x_validation, y_validation))
+    
+
+    return history, model1
+
+
