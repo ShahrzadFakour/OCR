ba032391189dd1432e5475d9447638e5043a91fc
ShahrzadFakour <66277015+ShahrzadFakour@users.noreply.github.com>
2022-02-02 23:42:53 +0100

    Update model.py

--- multiDigit/model.py
+++ multiDigit/model.py
@@ -12,12 +12,12 @@ def cnn_model():
     x = BatchNormalization()(model_input)
     x = Conv2D(32, (5, 5), activation='relu', padding='same')(model_input)
     x = Conv2D(32, (5, 5), activation='relu', padding='same')(x)
-    x = MaxPooling2D(pool_size=(2, 2))(x)
+    x = MaxPooling2D(pool_size=(2, 2),strides=(2,2),padding='same')(x)
     x = Dropout(0.25)(x)
 
     x = Conv2D(64, (5, 5), activation='relu', padding='same')(x)
     x = Conv2D(64, (5, 5), activation='relu', padding='same')(x)
-    x = MaxPooling2D(pool_size=(2, 2))(x)
+    x = MaxPooling2D(pool_size=(2, 2),strides=(2,2),padding='same')(x)
     x = Dropout(0.25)(x)
 
     x = Conv2D(128, (5, 5), activation='relu', padding='same')(x)
@@ -29,9 +29,10 @@ def cnn_model():
     x = Dense(256, activation='relu')(x)
     x = Dropout(.5)(x)
     y = [Dense(11, activation='softmax')(x) for i in range(5)]
+
     model = Model(inputs=model_input, outputs=y)
     #optimizer=tensorflow.keras.optimizers.Adam(lr=1e-3,amsgrad=True)
     model.compile(loss='categorical_crossentropy',
                   optimizer='adam', metrics=['accuracy'])
     model.summary()
-    return model
+    return model,y
